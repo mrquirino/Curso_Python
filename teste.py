6210eaa62dca87f74d22b1cd81e3584f7dd2ef79
mrquirino <matheusf730@gmail.com>
2024-04-23 12:59:25 -0300

    up

--- teste.py
+++ teste.py
@@ -1,2 +1,5 @@
-nome = input('Qual seu nome: ')
-print( nome, '! Prazer em te conhecer!')
+#Desafio 1, perguntar o nome e dar as boas vindas.
+
+'''nome = input('Qual seu nome: ')
+print( nome, '! Prazer em te conhecer!')'''
+
