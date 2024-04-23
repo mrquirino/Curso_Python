d8007eb2bdaece3f76ff078bc28c5a3b5db667e0
mrquirino <matheusf730@gmail.com>
2024-04-23 13:31:02 -0300

    Desafio2

--- teste.py
+++ teste.py
@@ -2,4 +2,12 @@
 
 '''nome = input('Qual seu nome: ')
 print( nome, '! Prazer em te conhecer!')'''
+#======================================================================
 
+#Desafio 2, ler a data de nascimento de uma pessoa e mostrar uma mesnagem com data formatada.
+
+'''print('Quando você nasceu??')
+dia = input('Qual o dia: ')
+mês = input('Qual o mês: ')
+ano = input('Qual o ano: ')
+print(dia,'de',mês,'de',ano, 'correto?')'''
